b1a17ec992bfb3dba384bf5b05a461db62c5df78
KayceeXclusive <dxclusive600@gmail.com>
2024-06-28 11:43:41 +0100

    delete button implementation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QMainWindow, QTableWidget, QToolBar, QDialog, QTableWidgetItem, QPushButton
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QMainWindow, QTableWidget, QToolBar, QDialog, QTableWidgetItem, QPushButton, QMessageBox, QComboBox, QStatusBar
 
 from PyQt6.QtCore import Qt 
 from PyQt6.QtGui import QAction
@@ -51,6 +51,11 @@ class MainWindow(QMainWindow):
         
         toolbar.addAction(add_products_action)
         toolbar.addAction(search_products_action)
+
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        self.table.cellClicked.connect(self.cell_clicked)
     
 
     def show_products(self):
@@ -61,6 +66,22 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.table)
         
 
+    def cell_clicked(self):
+        edit_button = QPushButton("Edith Record")
+        edit_button.clicked.connect(self.edit)
+
+        delete_button = QPushButton("Delete Record")
+        delete_button.clicked.connect(self.delete)
+
+
+        children = self.findChildren(QPushButton)
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+
+        self.statusbar.addWidget(edit_button)
+        self.statusbar.addWidget(delete_button)
+        
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
@@ -100,6 +121,135 @@ class MainWindow(QMainWindow):
         
         self.setCentralWidget(self.Transaction_table)
 
+    def edit(self):
+        dialog = EditDialog()
+        dialog.exec()
+
+    def delete(self):
+        dialog = DeleteDialog()
+        dialog.exec()
+
+
+class DeleteDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Delete Product")
+        self.setFixedWidth(200)
+        self.setFixedHeight(150)
+
+        layout = QGridLayout()
+        confirmation =QLabel("Are you sure you want to delete product ?")
+        yes = QPushButton("Yes")
+        no = QPushButton("No")
+
+        layout.addWidget(confirmation, 0, 0, 1, 2)
+        layout.addWidget(yes, 1, 0)
+        layout.addWidget(no, 1, 0)
+
+        self.setLayout(layout)
+
+        layout.setHorizontalSpacing(10)
+        layout.setVerticalSpacing(20)
+
+       
+        yes.clicked.connect(self.delete_product)
+        no.clicked.connect(self.reject)
+        layout.addWidget(yes)
+       
+            
+            
+        
+
+        self.setLayout(layout)
+
+    def delete_product(self):
+        index = main_window.table.currentRow()
+        pro_id = main_window.table.item(index, 0).text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM products WHERE PRODUCT_ID = ?", (pro_id))
+
+        connection.commit()
+        cursor.close
+        connection.close()
+
+        # Refresh the table
+        main_window.load_data()
+
+        self.close()
+
+        confirmation_widget = QMessageBox()
+        confirmation_widget.setWindowTitle("Sucess")
+        confirmation_widget.setText("The record was deleted successfully!")
+        confirmation_widget.exec()
+
+
+class EditDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Update Product Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        index = main_window.table.currentRow()
+        pro_Name = main_window.table.item(index, 1).text()
+
+        self.pro_id = main_window.table.item(index, 0).text()
+
+        self.pro_Name = QLineEdit(pro_Name)
+        self.pro_Name.setPlaceholderText("Product Name")
+        layout.addWidget(self.pro_Name)
+
+        pro_Cat = main_window.table.item(index, 2).text()
+        self.pro_Cat_Name = QComboBox()
+        pro_Cat_Grade =["Grade 1", "Grade 2", "Grade 3"]
+        self.pro_Cat_Name.addItems(pro_Cat_Grade)
+        self.pro_Cat_Name.setCurrentText(pro_Cat)
+        layout.addWidget(self.pro_Cat_Name)
+
+        pro_Sp = main_window.table.item(index, 3).text()
+        self.pro_Sp = QLineEdit(pro_Sp)
+        self.pro_Sp.setPlaceholderText("Selling Price")
+        layout.addWidget(self.pro_Sp)
+
+        pro_Cp = main_window.table.item(index, 4).text()
+        self.pro_Cp = QLineEdit(pro_Cp)
+        self.pro_Sp.setPlaceholderText("Cost Price")
+        layout.addWidget(self.pro_Cp)
+
+        pro_Quantity = main_window.table.item(index, 5).text()
+        self.pro_Quantity = QLineEdit(pro_Quantity)
+        self.pro_Quantity.setPlaceholderText("Quantity")
+        layout.addWidget(self.pro_Quantity)
+
+        pro_Des = main_window.table.item(index, 6).text()
+        self.pro_Des = QLineEdit(pro_Des)
+        self.pro_Des.setPlaceholderText("Description")
+        layout.addWidget(self.pro_Des)
+
+
+
+        button = QPushButton("Update")
+        button.clicked.connect(self.update_product)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def update_product(self):
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("update products SET NAME = ?, CATEGORY = ?, SELLING_PRICE = ?, COST_PRICE =?, QUANTITY = ?, DESCRIPTION = ? WHERE PRODUCT_ID = ?", (self.pro_Name.text(), self.pro_Cat_Name.itemText(self.pro_Cat_Name.currentIndex()), self.pro_Sp.text(), self.pro_Cp.text(), self.pro_Quantity.text(),self.pro_Des.text(), self.pro_id))
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
+
+
+
+
 class AddProducts(QDialog):
     def __init__(self):
         super().__init__()
@@ -113,9 +263,10 @@ class AddProducts(QDialog):
         self.pro_Name.setPlaceholderText("Product_Name")
         layout.addWidget(self.pro_Name)
 
-        self.pro_Cat = QLineEdit()
-        self.pro_Cat.setPlaceholderText("Product_Category")
-        layout.addWidget(self.pro_Cat)
+        self.pro_Cat_Name = QComboBox()
+        pro_Cat =["Grade 1", "Grade 2", "Grade 3"]
+        self.pro_Cat_Name.addItems(pro_Cat)
+        layout.addWidget(self.pro_Cat_Name)
 
         self.pro_Sp = QLineEdit()
         self.pro_Sp.setPlaceholderText("Selling Price")
@@ -142,11 +293,15 @@ class AddProducts(QDialog):
 
     def insert_product(self):
         name = self.pro_Name.text()
-        category = self.pro_Cat.text()
+        category = self.pro_Cat_Name.itemText(self.pro_Cat_Name.currentIndex())
         selling_price = self.pro_Sp.text()
         cost_price = self.pro_Cp.text()
         quantity = self.pro_Quantity.text()
         description = self.pro_Dec.text()
+
+        if not name or not category or not selling_price or not cost_price or not quantity or not description:
+            QMessageBox.warning(self, "Input Error", "All fields must be filled.")
+            return
 
         connection = sqlite3.connect("database.db")
         cursor = connection.cursor()
